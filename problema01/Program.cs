9386bdbe78ea25373c6ac3189ac9733e15f45fae
Emerson Vieira <emevieira@gmail.com>
2021-02-22 21:32:12 -0400

    Conclusao do problema 01

--- problema01/Program.cs
+++ problema01/Program.cs
@@ -1,4 +1,20 @@
-﻿using System;
+﻿/*
+Leia 2 valores inteiros e armazene-os nas variáveis A e B. 
+Efetue a soma de A e B atribuindo o seu resultado na variável X. 
+Imprima X conforme exemplo apresentado abaixo. Não apresente mensagem 
+alguma além daquilo que está sendo especificado e não esqueça de 
+imprimir o fim de linha após o resultado, caso contrário, você receberá 
+"Presentation Error".
+
+Entrada
+A entrada contém 2 valores inteiros.
+
+Saída
+Imprima a mensagem "X = " (letra X maiúscula) seguido pelo valor da 
+variável X e pelo final de linha. Cuide para que tenha um espaço antes 
+e depois do sinal de igualdade, conforme o exemplo abaixo.
+*/
+using System;
 
 namespace uri_algoritmos
 {
@@ -6,7 +22,13 @@ namespace uri_algoritmos
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            int A = Int32.Parse(Console.ReadLine());
+
+            int B = Int32.Parse(Console.ReadLine());
+
+            int X = A + B;
+            System.Console.WriteLine("X = " + X);
         }
     }
 }
