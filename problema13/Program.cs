ca643552a7d3f7082a566b459d8edf3fe96b89ea
Emerson Vieira <emevieira@gmail.com>
2021-05-12 22:48:35 -0400

    Resolucao do problema 13 - URI 1019

---
+++ problema13/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace problema13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int horas, minutos, segundos;
+
+            int N = Int32.Parse(Console.ReadLine());
+
+            minutos = N / 60;
+            segundos = N % 60;
+            horas = minutos / 60;
+            minutos %= 60;
+
+            System.Console.WriteLine("{0}:{1}:{2}", horas, minutos, segundos);
+
+        }
+    }
+}
